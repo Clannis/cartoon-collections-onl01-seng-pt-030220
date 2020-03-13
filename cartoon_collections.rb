d7aaf37975cdedb5b2f7bfc4eb1012e5a19cb5ee
James Dodds <dodds1990@gmail.com>
2020-03-13 00:26:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each do |name|
+    puts "#{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
@@ -14,3 +17,5 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+roll_call_dwarves(["Dopey", "Grumpoy", "Bashful"])
